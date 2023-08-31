b28bf39a07c57f8d93ac6093ebebd6e08d9aa7b1
Amir Shammasov <73101144+ShammasovAmir@users.noreply.github.com>
2023-09-01 00:49:49 +0300

    Added items and custom creative tabs

--- src/main/java/net/ash/tutorialmod/TutorialMod.java
+++ src/main/java/net/ash/tutorialmod/TutorialMod.java
@@ -1,5 +1,8 @@
 package net.ash.tutorialmod;
 
+import net.ash.tutorialmod.item.ModCreativeModTabs;
+import net.ash.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -14,12 +17,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
-public class TutorialMod
-{
+public class TutorialMod {
     public static final String MOD_ID = "tutorialmod";
-    public TutorialMod()
-    {
+
+    public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -28,23 +32,23 @@ public class TutorialMod
         modEventBus.addListener(this::addCreative);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
 
-    private void addCreative(CreativeModeTabEvent.BuildContents event)
-    {
-
+    private void addCreative(CreativeModeTabEvent.BuildContents event) {
+        if (event.getTab() == ModCreativeModTabs.TUTORIAL_TAB) {
+            event.accept(ModItems.ZIROCON);
+            event.accept(ModItems.BLACK_OPAL);
+            event.accept(ModItems.RAW_BLACK_OPAL);
+        }
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
+        public static void onClientSetup(FMLClientSetupEvent event) {
 
         }
     }
